2188b4641faf10638574c2da1c7d862dcb5dd65c
Sudharsshan <sudharsshansy@gmail.com>
2026-04-10 22:45:44 +0530

    werapped with intrinsic width widget to ensire the child get's finite width to calculate

--- lib/widgets/app_bar/app_bar_main.dart
+++ lib/widgets/app_bar/app_bar_main.dart
@@ -35,54 +35,59 @@ class _AppBarMainState extends State<AppBarMain>
   Widget build(BuildContext context) {
     final iconLength = IconsMap.barIcons.length;
 
-    return Stack(
-      children: [
-        // sliding background
-        AnimatedAlign(
-          curve: Curves.elasticOut,
-          alignment: Alignment(
-            -1.0 + (widget.activePage * (2.0 / (iconLength - 1))),
-            0,
-          ),
-          duration: const Duration(milliseconds: 300),
-          child: FractionallySizedBox(
-            widthFactor: 1 / iconLength,
-            child: Container(
-              margin: EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                color: Theme.of(
-                  context,
-                ).textTheme.bodySmall!.color!.withAlpha(40),
-                borderRadius: BorderRadius.circular(BlurRadius.radius),
+    return IntrinsicWidth(
+      child: Stack(
+        children: [
+          // sliding background
+          AnimatedAlign(
+            curve: Curves.elasticOut,
+            alignment: Alignment(
+              -1.0 + (widget.activePage * (2.0 / (iconLength - 1))),
+              0,
+            ),
+            duration: const Duration(milliseconds: 300),
+            child: FractionallySizedBox(
+              widthFactor: 1 / iconLength,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                margin: const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
+                decoration: BoxDecoration(
+                  color: Theme.of(
+                    context,
+                  ).textTheme.bodySmall!.color!.withAlpha(40),
+                  borderRadius: BorderRadius.circular(BlurRadius.radius),
+                ),
               ),
             ),
           ),
-        ),
 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: IconsMap.barIcons.entries.map((entry) {
-            bool isActive = widget.activePage == entry.value;
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: IconsMap.barIcons.entries.map((entry) {
+              bool isActive = widget.activePage == entry.value;
 
-            return Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              margin: const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
-              constraints: BoxConstraints(minWidth: 60),
-              child: IconButton(
-                onPressed: () {
-                  widget.pageNotifier.value = entry.value;
-                },
-                icon: FaIcon(
-                  entry.key,
-                  size: 20,
-                  color: isActive ? Colors.white : Colors.white.withAlpha(100),
+              return Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                margin: const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
+                constraints: BoxConstraints(minWidth: 60),
+                child: IconButton(
+                  onPressed: () {
+                    widget.pageNotifier.value = entry.value;
+                  },
+                  icon: FaIcon(
+                    entry.key,
+                    size: 20,
+                    color: isActive
+                        ? Colors.white
+                        : Colors.white.withAlpha(100),
+                  ),
                 ),
-              ),
-            );
-          }).toList(),
-        ),
-      ],
+              );
+            }).toList(),
+          ),
+        ],
+      ),
     );
   }
 }
