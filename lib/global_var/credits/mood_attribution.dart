30662ce2572570673fc43bb833d0a5de3daa8e4a
Sudharsshan <sudharsshansy@gmail.com>
2026-04-11 23:15:29 +0530

    added images for moods, created a map to store their attribution and image path separately.

---
+++ lib/global_var/credits/mood_attribution.dart
@@ -0,0 +1,20 @@
+class MoodAttribution {
+
+  static final Map<String, String> attribution = {
+    'Defiant' : 'Photo by BAILEY MAHON on Unsplash',
+    'Aggressive' : 'Photo by Anna Popović on Unsplash',
+    'Energizing' : 'Photo by Gio Bartlett on Unsplash',
+    'Easygoing' : 'Photo by ÉMILE SÉGUIN 🧩🧩🧩 on Unsplash',
+    'Empowering' : 'Photo by visuals on Unsplash',
+    'Excited' : 'Photo by Japheth Mast on Unsplash',
+    'Fiery' : 'Photo by Mladen Borisov on Unsplash',
+    'Gritty' : 'Photo by Shees Bin Shahid on Unsplash',
+    'Peaceful' : 'Photo by Jeffery Erhunse on Unsplash',
+    'Romantic' : 'Photo by Farrinni on Unsplash',
+    'Rowdy' : 'Photo by Barnabas Davoti on Unsplash',
+    'Sensual' : 'Photo by Zhu Yunxiao on Unsplash',
+    'Sentimental' : 'Photo by Nicholas Ng on Unsplash',
+    'Stirring' : 'Photo by S H A R EE Q A on Unsplash',
+    'Upbeat' : 'Photo by Ahmed Zayan on Unsplash'
+  };
+}
---
+++ lib/global_var/links/mood_covers.dart
@@ -0,0 +1,20 @@
+class MoodCovers {
+  // A class to store the path to each image based on their moods.
+  static final Map<String, String> coverArtLinks = {
+  'Aggressive': 'lib/assets/mood_covers/Aggressive.jpg',
+  'Defiant': 'lib/assets/mood_covers/Defiant.jpg', // Matched lowercase 'd' from your list
+  'Easygoing': 'lib/assets/mood_covers/Easygoing.jpg',
+  'Empowering': 'lib/assets/mood_covers/Empowering.jpg',
+  'Energizing': 'lib/assets/mood_covers/Energizing.jpg',
+  'Excited': 'lib/assets/mood_covers/Excited.jpg',
+  'Fiery': 'lib/assets/mood_covers/Fiery.jpg',
+  'Gritty': 'lib/assets/mood_covers/Gritty.jpg',
+  'Peaceful': 'lib/assets/mood_covers/Peaceful.jpg',
+  'Romantic': 'lib/assets/mood_covers/Romantic.jpg',
+  'Rowdy': 'lib/assets/mood_covers/Rowdy.jpg',
+  'Sensual': 'lib/assets/mood_covers/Sensual.jpg',
+  'Sentimental': 'lib/assets/mood_covers/Sentimental.jpg',
+  'Stirring': 'lib/assets/mood_covers/Stirring.jpg',
+  'Upbeat': 'lib/assets/mood_covers/Upbeat.jpg',
+};
+}
