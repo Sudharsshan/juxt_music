1d24ad860546beb3001aca43a191c5b0ac2f9978
Sudharsshan <sudharsshansy@gmail.com>
2026-04-19 12:51:03 +0530

    updated website build with newer commit changes.

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "310708200" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3268517607" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
