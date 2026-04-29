0165bc91c25c8cfb6148c8d7473fef36dbcc42a0
Sudharsshan <sudharsshansy@gmail.com>
2026-04-29 20:09:28 +0530

    refactored que page to follow the theme of control page. Also added icons for making track favorite and more options icon button widget.

--- lib/widgets/music_player/pages/queue_page.dart
+++ lib/widgets/music_player/pages/queue_page.dart
@@ -16,9 +16,7 @@ class QueuePage extends StatelessWidget {
           child: ClipRRect(
             child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
-              child: Container(
-                color: Colors.white.withAlpha(25),
-              ),
+              child: Container(color: Colors.white.withAlpha(25)),
             ),
           ),
         ),
@@ -30,7 +28,7 @@ class QueuePage extends StatelessWidget {
             final currentIndex = musicQueState.currentIndex;
 
             return ListView.builder(
-              padding: const EdgeInsets.only(top: 80, bottom: 20),
+              padding: const EdgeInsets.only(top: 110, bottom: 20),
               itemCount: tracks.length,
               itemBuilder: (context, index) {
                 final track = tracks[index];
@@ -39,7 +37,9 @@ class QueuePage extends StatelessWidget {
                 return ListTile(
                   leading: ClipRRect(
                     borderRadius: BorderRadius.circular(8.0),
-                    child: (track.preferredArtwork != null && track.preferredArtwork!.startsWith('http'))
+                    child:
+                        (track.preferredArtwork != null &&
+                            track.preferredArtwork!.startsWith('http'))
                         ? Image.network(
                             track.preferredArtwork!,
                             width: 50,
@@ -52,22 +52,58 @@ class QueuePage extends StatelessWidget {
                   ),
                   title: Text(
                     track.title,
-                    style: TextStyle(
-                      fontWeight: isPlaying ? FontWeight.bold : FontWeight.normal,
-                      color: isPlaying ? Theme.of(context).primaryColor : Theme.of(context).textTheme.bodyLarge?.color,
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontSize: 12,
+                      fontWeight: isPlaying
+                          ? FontWeight.bold
+                          : FontWeight.normal,
+                      color: isPlaying
+                          ? Theme.of(context).primaryColor
+                          : Theme.of(context).textTheme.bodyLarge?.color,
                     ),
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                   ),
                   subtitle: Text(
                     track.artistName,
-                    style: TextStyle(
-                      color: isPlaying ? Theme.of(context).primaryColor.withAlpha(200) : Theme.of(context).textTheme.titleMedium?.color,
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      fontSize: 12,
+                      color: isPlaying
+                          ? Theme.of(context).primaryColor.withAlpha(200)
+                          : Theme.of(context).textTheme.titleMedium?.color,
                     ),
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                   ),
-                  trailing: isPlaying ? Icon(Icons.volume_up, color: Theme.of(context).primaryColor) : null,
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      if (isPlaying)
+                        Icon(
+                          Icons.volume_up,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.star_border,
+                            color: Theme.of(
+                              context,
+                            ).textTheme.titleMedium!.color,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.more_vert,
+                          color: Theme.of(context).textTheme.titleMedium!.color,
+                        ),
+                      ),
+                    ],
+                  ),
                   onTap: () {
                     musicQueState.playTrackById(track.preview.id);
                   },
